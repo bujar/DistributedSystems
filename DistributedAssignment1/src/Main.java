c8b64c68a1acc6756ddc02417b34cf1aaa395e9f
Bujar <btagani@cmu.edu>
2015-01-23 18:46:44 -0500

    New YML file and new method for parsing YML

---
+++ DistributedAssignment1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		//will change to retrieving paramaters from user args
+		MessagePasser msg = new MessagePasser("configuration.yml", "local");
+	}
+}
--- DistributedAssignment1/src/MessagePasser.java
+++ DistributedAssignment1/src/MessagePasser.java
@@ -1,4 +1,12 @@
+import java.awt.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
 
 
 public class MessagePasser {
@@ -9,11 +17,28 @@ public class MessagePasser {
 	
 	public MessagePasser(String pathName, String localName){
 		seqNum = 0;
+
+		Map<String, Object> data = getYamlData(pathName);
+
 		// for host in configFile
 		//new Socket host
 		Host h = new Host(null, null, null);
 		hostList.add(h);
+
+	}
+	
+	public Map<String, Object> getYamlData(String pathName){
+		InputStream input = null;
+		try {
+			input = new FileInputStream(new File(
+			        pathName));
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find configuration file");
+			e.printStackTrace();
+		}
+		Yaml yaml = new Yaml();
+		Map<String, Object> configData = (Map<String, Object>) yaml.load(input);
+		return configData;
 	}
 	
 }
-
