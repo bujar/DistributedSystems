38dc598894cfdc2d6020b84cc46fb6af799b5116
keenlooks <keenlooks@gmail.com>
2015-01-23 13:54:28 -0500

    changedMessage

--- DistributedAssignment1/src/Message.java
+++ DistributedAssignment1/src/Message.java
@@ -1,4 +1,17 @@
 
 public class Message {
- //blabla
+    public Message(String dest, String kind, Object data){
+        
+    }
+    
+    public void set_source(String source){
+        
+    }
+    
+    public void set_seqNum(int sequenceNumber){
+        
+    }
+    public void set_duplicate(Boolean dupe){
+        
+    }
 }
