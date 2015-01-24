d27ef7144221a2127f4f7b1b79d6e9b67a70c8b1
Bujar <btagani@cmu.edu>
2015-01-23 22:36:43 -0500

    done parsing. Sockets not working as expected
    I can’t really start a new socket and then just leave it there to
    access later. Once the socket starts, it blocks that thread. Maybe give
    a separate thread to each connection? Not sure what to do atm.

--- DistributedAssignment1/src/MessagePasser.java
+++ DistributedAssignment1/src/MessagePasser.java
@@ -2,7 +2,10 @@ import java.awt.List;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -17,17 +20,25 @@ public class MessagePasser {
 	
 	public MessagePasser(String pathName, String localName){
 		seqNum = 0;
+		
+		Map<String, ArrayList<Map<String, Object>>> data = getYamlData(pathName);
+		ArrayList<Map<String, Object>> config = data.get("configuration");
 
-		Map<String, Object> data = getYamlData(pathName);
-
-		// for host in configFile
-		//new Socket host
-		Host h = new Host(null, null, null);
-		hostList.add(h);
-
+		for (Map<String, Object> key : config){
+			
+			String name = (String) key.get("name");
+			String ipAddr = (String) key.get("ip");
+			 int port = (Integer) key.get("port");
+			Integer p = Integer.valueOf(port);
+			Socket connection = newConnection(ipAddr, p);
+			Host host = new Host(name, connection, ipAddr);
+			hostList.add(host);
+			
+		}
+		System.out.println(hostList);
 	}
 	
-	public Map<String, Object> getYamlData(String pathName){
+	public Map<String, ArrayList<Map<String, Object>>> getYamlData(String pathName){
 		InputStream input = null;
 		try {
 			input = new FileInputStream(new File(
@@ -37,8 +48,21 @@ public class MessagePasser {
 			e.printStackTrace();
 		}
 		Yaml yaml = new Yaml();
-		Map<String, Object> configData = (Map<String, Object>) yaml.load(input);
+		Map<String, ArrayList<Map<String, Object>>> configData = (Map<String, ArrayList<Map<String, Object>>>) yaml
+				.load(input);
 		return configData;
+	}
+	
+	public Socket newConnection(String ipAddr, int port) {
+		Socket connection = null;
+		try {
+			connection = new Socket(ipAddr, port);
+		} catch (UnknownHostException e) {
+			System.out.println("Could not establish connection to " + ipAddr);
+		} catch (IOException e) {
+			System.out.println("Could not establish connection to " + ipAddr);
+		}
+		return connection;
 	}
 	
 }
