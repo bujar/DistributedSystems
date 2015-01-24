c74dd1b697c9782854db8e33ce0120b14e02f4ef
keenlooks <keenlooks@gmail.com>
2015-01-23 19:09:10 -0500

    update SendMessage

--- DistributedAssignment1/src/SendMessage.java
+++ DistributedAssignment1/src/SendMessage.java
@@ -1,7 +1,14 @@
 
+import java.util.Set;
+
+
 public class SendMessage {
+    public int seqNum;
     public void send(Message m){
-        //checkrules
+        
+        //set message contents
+        m.set_seqNum(seqNum);
+        seqNum++;
         for(int i=0; i < hostList.length(); i++){
             if(hostList.get(i).name == m.dest){
                 hostList.get(i).sock.send(m);
