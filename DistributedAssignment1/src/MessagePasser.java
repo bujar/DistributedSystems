21706cc48b5735922c372445715296f071d03d21
keenlooks <keenlooks@gmail.com>
2015-02-10 17:46:57 -0500

    fixed send

--- DistributedAssignment1/src/MessagePasser.java
+++ DistributedAssignment1/src/MessagePasser.java
@@ -31,7 +31,7 @@ public class MessagePasser {
 	public ArrayList<Rule> recvRules = new ArrayList<Rule>();
 	public int seqNum;
 	public LinkedList<TimeStampedMessage> recvDelayQueue;
-	public LinkedList<Message> sendDelayQueue;
+	public LinkedList<TimeStampedMessage> sendDelayQueue;
 	public static String configFile;
 	public static String configURL;
 	public String localSource;
@@ -314,7 +314,8 @@ public class MessagePasser {
 												.println("dropped--------------------------------");
 										return;
 									} else if (action.equals("delay")) {
-										sendDelayQueue.add(m);
+										sendDelayQueue.add(new TimeStampedMessage(m,
+														clock.getTimestamp()));
 										// System.out.println("delayed message "
 										// + m.data);
 										System.out
@@ -347,13 +348,13 @@ public class MessagePasser {
 				hostList.get(i).sock.send(new TimeStampedMessage(m, clock
 						.getTimestamp()));
 				while (!sendDelayQueue.isEmpty()) {
-					Message delayedMessage = sendDelayQueue.poll();
+					TimeStampedMessage delayedMessage = sendDelayQueue.poll();
 					delayedMessage.set_seqNum(seqNum);
 					seqNum++;
 					for (int j = 0; j < hostList.size(); j++) {
 						if (hostList.get(j).name.equals(delayedMessage.dest)) {
-							hostList.get(j).sock.send(new TimeStampedMessage(
-									delayedMessage, clock.getTimestamp()));
+							hostList.get(j).sock.send(
+									delayedMessage);
 
 						}
 					}
