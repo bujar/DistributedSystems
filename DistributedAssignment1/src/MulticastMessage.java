cff92ecf3dd0aeb73ef6b5eac656ef0b823727cb
keenlooks <keenlooks@gmail.com>
2015-02-11 21:20:37 -0500

    MulticastMessage class
    extends TimeStampedMessage. contains methods to take in Acks for it and
    a method to return the time in milliseconds it has been since it has
    been received (can be used to check how long it has been waiting for
    acks in the queue)

---
+++ DistributedAssignment1/src/MulticastMessage.java
@@ -0,0 +1,44 @@
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MulticastMessage extends TimeStampedMessage implements Serializable {
+
+    private static final long serialVersionUID = 1529127835408294642L;
+    Group group;
+    long timeReceived = 0;
+    ArrayList<TimeStampedMessage> acksReceived;
+    
+    public MulticastMessage(String newdest, String newkind, Object newdata, TimeStamp newstamp, Group newgroup) {
+        super(newdest, newkind, newdata, newstamp);
+        group = newgroup;
+        timeReceived = System.currentTimeMillis();
+        acksReceived = new ArrayList<TimeStampedMessage>();
+    }
+
+    public MulticastMessage(TimeStampedMessage m, Group newgroup) {
+        super(m.getMessage(),m.stamp);
+        group = newgroup;
+        timeReceived = System.currentTimeMillis();
+    }
+    
+    //adds ack to acklist
+    public void addAck(TimeStampedMessage newack){
+        for(int i = 0; i < acksReceived.size(); i++){
+            if(acksReceived.get(i).source.equals(newack.source)){
+               return; 
+            }
+        }
+        acksReceived.add(newack);
+    }
+    
+    //will return true if the number of acks received equals the number of members in the group (means it also inludes own ack if self is in group)
+    public boolean fullyAcked(){
+        return acksReceived.size() == group.members.size();
+    }
+    
+    //returns time passes since this message was received. will be used to determine if Ack has been missing too long from another node
+    public long getTimePassed(long currentTime){
+        return currentTime-timeReceived;
+    }
+}
